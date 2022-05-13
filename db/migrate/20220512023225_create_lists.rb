aaad2e505fe5900ace9b68c51fd798f1e68e3f13
shingo kubomura <skksb75@gmail.com>
2022-05-13 12:04:53 +0900

    Add dotenv - Protect my secret data in .env file

--- db/migrate/20220512023225_create_lists.rb
+++ db/migrate/20220512023225_create_lists.rb
@@ -3,6 +3,7 @@ class CreateLists < ActiveRecord::Migration[6.1]
     create_table :lists do |t|
       t.string :name
 
+
       t.timestamps
     end
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# require 'open-uri'
+# require 'faker'
+
+# url = 'http://tmdb.lewagon.com/movie/top_rated'
+# response = JSON.parse(URI.open(url).read)
+
+# puts "Cleaning the DB..."
+# Movie.destroy_all
+
+# puts "Creating movies..."
+# response['results'].each do |movie_hash|
+#   Movie.create!(
+#     title: Faker::Movie.title,
+#     overview: "#{Faker::Movie.title}: #{Faker::Movie.quote}",
+#     poster_url: "https://image.tmdb.org/t/p/w500" + movie_hash['poster_path'],
+#     rating: Faker::Number.decimal(l_digits: 1)
+#   )
+# end
+# puts "...created #{Movie.count} movies."
+
+require 'open-uri'
+Bookmark.destroy_all
+Movie.destroy_all
+# List.destroy_all
+
+# the Le Wagon copy of the API
+url = 'http://tmdb.lewagon.com/movie/top_rated'
+response = JSON.parse(URI.open(url).read)
+
+response['results'].each do |movie_hash|
+
+  Movie.create(
+    title: movie_hash["title"],
+    overview: movie_hash["overview"],
+    rating: movie_hash["vote_average"],
+    poster_url: "https://image.tmdb.org/t/p/w500" + movie_hash['poster_path']
+  )
+end
+
+puts "...created #{Movie.count} movies."
