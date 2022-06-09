92b2d8c39e90e279c8957239a590a76fb62010f0
shingo kubomura <skksb75@gmail.com>
2022-06-09 11:47:44 +0700

    made clickable card list

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,10 +1,10 @@
 class ListsController < ApplicationController
+  before_action :find_list, only: [:show, :destroy]
   def index
     @lists = List.all
   end
 
   def show
-    @list = List.find(params[:id])
   end
 
   def new
@@ -20,9 +20,18 @@ class ListsController < ApplicationController
     end
   end
 
+  def destroy
+    @list.destroy
+    redirect_to lists_path
+  end
+
   private
 
   def list_params
     params.require(:list).permit(:name, :photo)
+  end
+
+  def find_list
+    @list = List.find(params[:id])
   end
 end
